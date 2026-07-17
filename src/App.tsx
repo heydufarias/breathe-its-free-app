b5946e0b7189ae40e61fc36cf2cfb0c19f9c2d2d
Eduardo Farias <heydufarias@gmail.com>
2026-07-17 19:41:11 -0300

    feat: Add Language type

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
+import { useTranslation } from 'react-i18next';
+import type { Language } from './lib/types';
+
 function App() {
+  const { t, i18n } = useTranslation();
+  const changeLanguage = (language: Language) => {
+    i18n.changeLanguage(language);
+  };
+
   return (
-    <div className="min-h-screen flex items-center justify-center bg-slate-900">
-      <h1 className="text-3xl font-bold text-white">breathe, it's free</h1>
+    <div className="min-h-screen flex flex-col items-center justify-center gap-6 bg-slate-900">
+      <h1 className="text-3xl font-bold text-white font-helvetica">
+        {t('home')}
+      </h1>
+
+      <div className="flex gap-2">
+        <button onClick={() => changeLanguage('pt-BR')}>PT-BR</button>
+        <button onClick={() => changeLanguage('en')}>EN</button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
