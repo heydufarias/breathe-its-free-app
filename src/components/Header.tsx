87f717f4fbadb3d10a71e35a2493e5ee9ae7203c
Eduardo Farias <heydufarias@gmail.com>
2026-07-31 18:21:22 -0300

    feat: Update Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-export function Header() {
+export function Header({ showInfoButton, onInfoButtonClick }: { showInfoButton: boolean, onInfoButtonClick: () => void }) {
   return (
     <header className="absolute top-0 left-0 right-0 z-10 flex items-center justify-between px-[clamp(1.5rem,5vw,2.5rem)] pt-4">
       <div className="flex flex-col text-4xl font-semibold tracking-tight leading-7">
@@ -6,11 +6,14 @@ export function Header() {
         <span>it is free.</span>
       </div>
 
-      <button
-        className="text-2xl font-semibold cursor-pointer"
-      >
-        Info
-      </button>
+      {showInfoButton && (
+        <button
+          onClick={onInfoButtonClick}
+          className="text-2xl font-semibold cursor-pointer"
+        >
+          Info
+        </button>
+      )}
 
     </header>
   );
