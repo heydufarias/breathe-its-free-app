435973841aca99d3731380dcbbc2841cadb8ca24
Eduardo Farias <heydufarias@gmail.com>
2026-08-10 21:27:56 -0300

    style: Reorder class names in Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 export function Header({ showInfoButton, onInfoButtonClick }: { showInfoButton: boolean, onInfoButtonClick: () => void }) {
   return (
-    <header className="absolute top-0 left-0 right-0 z-10 flex items-center justify-between px-[clamp(1.5rem,5vw,2.5rem)] pt-4">
+    <header className="absolute top-0 left-0 right-0 flex items-center justify-between px-[clamp(1.5rem,5vw,2.5rem)] pt-4 z-10">
       <div className="flex flex-col text-4xl font-semibold tracking-tight leading-7">
         <span>Breathe,</span>
         <span>it is free.</span>
