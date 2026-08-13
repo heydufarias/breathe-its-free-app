2223c565b4354024a1d112c1905637f8f72f9393
Eduardo Farias <heydufarias@gmail.com>
2026-08-13 03:18:50 -0300

    feat: Add MainButton component with start/finish functionality and translations

--- src/components/MainButton.tsx
+++ src/components/MainButton.tsx
@@ -4,21 +4,23 @@ import { cn } from "../lib/utils";
 import { solidBgVariants } from "../lib/variants";
 import type { BreathMode } from "../lib/types";
 
-interface StartButtonProps {
+interface MainButtonProps {
   currentMode: BreathMode;
   onStart: () => void;
+  showFinishButton: boolean;
+  onFinish: () => void;
 }
 
-export function StartButton({ currentMode, onStart }: StartButtonProps) {
+export function MainButton({ currentMode, onStart, showFinishButton, onFinish }: MainButtonProps) {
   const { t } = useTranslation();
 
   return (
     <button
-      onClick={onStart}
+      onClick={showFinishButton ? onFinish : onStart}
       className="flex h-18 flex-19 items-center justify-between p-1 bg-white rounded-full cursor-pointer"
     >
       <span className="pl-7 text-2xl transition-colors duration-500">
-        {t("Start")}
+        {showFinishButton ? t("Finish") : t("Start")}
       </span>
       <span
         className={cn(
