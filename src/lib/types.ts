39906a47a4bea2fb238bc8a011700344e43e9ce5
Eduardo Farias <heydufarias@gmail.com>
2026-08-19 21:17:49 -0300

    feat: Update SessionStage type

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -2,6 +2,6 @@ export type Language = 'en' | 'pt-BR';
 
 export type BreathMode = 'relax' | 'focus' | 'sleep'
 
-export type SessionStage = "idle" | "active" | "done";
+export type SessionStage = "idle" | "prepare" | "active" | "done";
 
 export type BreathPhaseLabel = "inhale" | "hold" | "exhale";
