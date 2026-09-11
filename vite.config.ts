81508f4f8763c995ce0c460f2b73cd2d300894c9
Eduardo Farias <heydufarias@gmail.com>
2026-09-11 20:38:10 -0300

    chore: Set base path for deployment to breathe-it-is-free-app

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/breathe-it-is-free-app/',
   plugins: [react(), tailwindcss()],
 })
